4b55eb79c9d944f30e9346a09cd484d9092c1c40
ch4ser-go <ch4ser.go@gmail.com>
2021-10-28 16:38:33 +0800

    add sessions_serialize_php

--- session_serialize_shell/files/src/flag.php
+++ session_serialize_shell/files/src/flag.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
-echo 'only localhost can get flag';
-if ($_SERVER['REMOTE_ADDR'] === '127.0.0.1') {
+if ($_SERVER["REMOTE_ADDR"] === "127.0.0.1") {
     if(!preg_match('/[a-z0-9]/is',$_GET['i'])) {
         eval($_GET['i']);
     }else{
         die('go away');
     }
+} else {
+    die( 'only localhost can get flag!');
 }
-
+?>
